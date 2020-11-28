b3fa625dfa4a11077ef5e2179d10fc8e4b1da46f
Nico Rehwaldt <git_nikku@nixis.de>
2020-11-28 18:43:39 +0100

    chore: improve type definitions

--- lib/types.d.ts
+++ lib/types.d.ts
@@ -1,11 +1,49 @@
-export type ProviderType = 'value' | 'factory' | 'type' | 'private';
+export type ValueType = 'value';
+export type FactoryType = 'factory';
+export type TypeType = 'type';
+
+export type ProviderType = ValueType | FactoryType | TypeType;
+
+export type InjectAnnotated = {
+  $inject?: string[];
+};
+
+export type ScopeAnnotated = {
+  $scope?: string[];
+};
+
+export type Annotated = InjectAnnotated & ScopeAnnotated;
+
+export type Constructor<T extends unknown> = {
+  new (...args: any[]): T;
+  (...args: any[]): T;
+} & Annotated;
+
+export type FactoryFunction<T extends unknown> = {
+  (...args: any[]): T;
+} & Annotated;
+
+export type ArrayArgs<T> = [ ...string[], T ];
+
+export type ServiceProvider<T extends unknown> = {
+  (name: string): T
+};
+
+export type FactoryDeclaration = FactoryFunction | ArrayArgs<FactoryFunction>;
+
+export type TypeDeclaration = Constructor | ArrayArgs<Constructor>;
+
+export type ValueDeclaration = any;
+
+type Declaration<T, D> = [ T, D ] | [ T, D, 'private' ];
 
 export type ServiceDeclaration =
-  [ ProviderType, any ] |
-  [ function(string) : any, string, ProviderType, any ];
+  Declaration<ValueType, ValueDeclaration> |
+  Declaration<TypeType, TypeDeclaration> |
+  Declaration<FactoryType, FactoryDeclaration>;
 
 export type ModuleDeclaration = {
   __exports__?: string[],
   __modules__?: ModuleDeclaration[],
-  [x: string]: ServiceDeclaration
+  [name: string]: ServiceDeclaration | ModuleDeclaration[] | string[]
 };
