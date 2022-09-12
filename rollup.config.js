def95ea0a8b6c4d008d526183afac501913767f5
Nico Rehwaldt <git_nikku@nixis.de>
2022-09-12 13:28:56 +0200

    chore: drop UMD build
    Modern browsers can consume the ES module build.

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,3 @@
-import { terser } from 'rollup-plugin-terser';
-
 import pkg from './package.json';
 
 function pgl(plugins = []) {
@@ -8,32 +6,10 @@ function pgl(plugins = []) {
 
 const srcEntry = pkg.source;
 
-const umdDist = pkg['umd:main'];
-
-const umdName = 'Didi';
-
 export default [
-
-  // browser-friendly UMD build
-  {
-    input: srcEntry,
-    output: {
-      file: umdDist.replace(/\.js$/, '.prod.js'),
-      format: 'umd',
-      name: umdName
-    },
-    plugins: pgl([
-      terser()
-    ])
-  },
   {
     input: srcEntry,
     output: [
-      {
-        file: umdDist,
-        format: 'umd',
-        name: umdName
-      },
       { file: pkg.main, format: 'cjs' },
       { file: pkg.module, format: 'es' }
     ],
